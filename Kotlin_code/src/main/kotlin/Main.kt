422554a4f0a5765b9a8fd301e287644a5c3de922
MrShashankBisht <shashank.singh2016.ss@gmail.com>
2024-07-06 13:08:05 +0530

    adding main and kotlin variable file

--- Kotlin_code/src/main/kotlin/Main.kt
+++ Kotlin_code/src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package org.example
 
+
+/*
+* 1) variables / Array / Matrix / HashMap / ArrayList / Set /
+* 2) functions / Scope Functions / misllis..
+* 3) loop and conditions
+* 4) Classes / Objects / classes type
+* 5) Threading
+* 6) OOPs concept
+* 7)
+* */
+
 fun main() {
     println("Hello World!")
 }
--- Kotlin_code/src/main/kotlin/Variables.kt
+++ Kotlin_code/src/main/kotlin/Variables.kt
@@ -1,2 +1,67 @@
 package org.example
 
+fun main() {
+//    val temp = "sha"
+//    var temp2 = 10
+//
+//    temp2 = 20
+//
+//    // define type of any variable
+//    val image: Int = 10
+//    var ele: String = "sha"
+//
+//    printNameOnly("shashank", "sha")
+//
+//    // hashmap, Set, and ArrayList
+//
+//    var map = mutableMapOf<String, Any>()
+//    map["name"] = "Shashank"
+//    map["age"] = 23
+//
+//    map.map { it ->
+//        println(it.value)
+//    }
+//
+//    val set = mutableSetOf<Int>()
+//    set.add(10)
+//    set.add(10)
+//    set.add(10)
+//    set.add(10)
+//
+//    set.map {
+//        println("print $it")
+//    }
+
+    data class Pixel(val red: Int, val green: Int, val blue: Int)
+
+//    val pixelArray: Array<Pixel> = Array(10) { Pixel(
+//        (0..255).random(),
+//        (0..255).random(),
+//        (0..255).random())
+//    }
+//
+//    for(i in 0..9){
+//        print(pixelArray[i]) // this will print "Pixel(red=..., green=..., blue=...)"
+//    }
+
+
+    val pixArray: Array<Array<Int?>> = Array (10) {
+        Array (10) {
+            0
+        }
+    }
+
+    for (i in 0 until 10) {
+        for(j in 0 until 10) {
+            print(pixArray[i][j])
+        }
+        println()
+    }
+}
+
+
+
+fun printNameOnly(name: String, partString: String) {
+    val isContains = name.contains(partString)
+    println(isContains)
+}
